fbe2e730d2bd409ea07395ca9eb6b9583798d701
aniya-webdev <aniya-webdev@yandex.ru>
2023-03-22 15:42:14 +0300

    release

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.header-nav');
+const navLinks = document.querySelectorAll('.header-nav-link');
+
+navIcon.addEventListener('click', function () {
+	this.classList.toggle('nav-icon--active');
+	nav.classList.toggle('nav--active');
+});
+
+navLinks.forEach(function (item, index) {
+	item.addEventListener('click', function () {
+		navIcon.classList.remove('nav-icon--active');
+		nav.classList.remove('nav--active');
+	});
+
+	item.addEventListener('keydown', function (event) {
+		if (nav.classList.contains('nav--active') && event.keyCode === 9) {
+			if (event.shiftKey) {
+				if (index === 0) {
+					event.preventDefault();
+					navIcon.focus();
+				}
+			} else {
+				if (index === navLinks.length - 1) {
+					event.preventDefault();
+					navIcon.focus();
+				}
+			}
+		}
+	});
+});
+
+navIcon.addEventListener('keydown', function (event) {
+	if (nav.classList.contains('nav--active') && event.keyCode === 9) {
+		event.preventDefault();
+		navLinks[0].focus();
+	}
+});
+
+document.addEventListener('keydown', function (event) {
+	if (event.key === 'Escape') {
+		navIcon.classList.remove('nav-icon--active');
+		nav.classList.remove('nav--active');
+	}
+});
+//# sourceMappingURL=main.js.map
